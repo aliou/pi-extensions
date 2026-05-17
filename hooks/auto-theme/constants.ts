28bbf4e1d22296e6ee884217ef450435d3ba1821
Aliou Diallo <code@aliou.me>
2026-05-17 17:47:45 +0200

    fix(theme): Use correct auto themes

--- hooks/auto-theme/constants.ts
+++ hooks/auto-theme/constants.ts
@@ -1,10 +1,10 @@
 /** Auto-theme constants. */
 
 /** Theme name to use when the terminal is in dark mode. */
-export const THEME_DARK = "dark";
+export const THEME_DARK = "jellybeans-mono";
 
 /** Theme name to use when the terminal is in light mode. */
-export const THEME_LIGHT = "light";
+export const THEME_LIGHT = "jellybeans-mono-light";
 
 /** How long to wait for a Mode 2031 query response before giving up. */
 export const QUERY_TIMEOUT_MS = 1500;
