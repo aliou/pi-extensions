a0cb6021030ab9d18937106b392edb55fe656097
Aliou Diallo <code@aliou.me>
2026-06-03 09:32:56 +0200

    chore: remove nvim undo support

--- packages/nvim-undofile/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { computeUndoHash, contentToUndoLines, undoLineCount } from "./hash";
-export { parseUndofile } from "./parse";
-export { DEFAULT_UNDODIR, getUndoFilePath, normalizeUndodir } from "./path";
-export { serializeUndofile } from "./serialize";
-export type { UpdateUndofileResult } from "./types";
-export {
-  appendUndoEntryToUndofile,
-  updateUndofileForExternalWrite,
-} from "./update";
